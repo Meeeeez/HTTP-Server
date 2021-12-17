29eda146043cb504fcef1925b89f660bdfc10412
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-12-17 17:05:45 +0100

    Using ServerSockets to serve a static HTML page

--- src/main/java/com/moritzmessner/http_server/HTTPServer.java
+++ src/main/java/com/moritzmessner/http_server/HTTPServer.java
@@ -2,6 +2,12 @@ package com.moritzmessner.http_server;
 
 import com.moritzmessner.http_server.config.Configuration;
 import com.moritzmessner.http_server.config.ConfigurationManager;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPServer {
 
@@ -12,5 +18,39 @@ public class HTTPServer {
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfig();
 
         System.out.print("Port: " + conf.getPort() + "\nWeb Root: " + conf.getWebroot() + "\n");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String HTMLFileContent;
+            try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/html/index.html")) {
+                HTMLFileContent = new String(fileInputStream.readAllBytes(), StandardCharsets.UTF_8);
+            }
+
+            String CRLF = "\n\r"; //carriage return, line feed
+
+            final String HTTPResponse =
+                    "HTTP/1.1 200 OK" +                                                     //HTTP version + status code
+                            CRLF +
+                            "Content-Lenght: " + HTMLFileContent.getBytes().length + CRLF + //header
+                            CRLF +
+                            HTMLFileContent +                                               //content
+                            CRLF + CRLF;
+
+            outputStream.write(HTTPResponse.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            serverSocket.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
